fbfe23074293a7017f0d6909104a03b06999ce3c
Nikita_Pl <bruuix@gmail.com>
2025-10-17 13:31:02 +0700

    Added weatherTempInfo

--- src/components/WeatherSearch.jsx
+++ src/components/WeatherSearch.jsx
@@ -1,5 +1,9 @@
 import styles from "./WeatherSearch.module.css";
-import weatherImage from "../assets/images/Weather_image.png"
+import weatherImage from "../assets/images/weather.png";
+import cloudRain from "../assets/images/cloudRain.png"
+import temperatureMin from "../assets/images/temperatureMin.png"
+import temperatureMax from "../assets/images/temperatureMax.png"
+
 
 function WeahterSearch() {
   return (
@@ -13,10 +17,18 @@ function WeahterSearch() {
         ></input>
       </form>
       <div className={styles.currentWeather}>
-        <img className={styles.currentWeatherImg} src={weatherImage}/>
+        <img className={styles.currentWeatherImg} src={weatherImage} />
         <h1 className={styles.currentTemp}>30C</h1>
         <p className={styles.currentCity}>Tomsk</p>
         <p className={styles.currentDay}>Monday</p>
+      </div>
+      <div className={styles.currentWeatherTempInfo}>
+        <img src={cloudRain} alt={cloudRain} />
+        <p>Light Rain</p>
+        <img src={temperatureMin} alt={temperatureMin} />
+        <p>Min Temperature - 28°C</p>
+        <img src={temperatureMax} alt={temperatureMax} />
+        <p>Max Temperature - 31°C</p>
       </div>
     </section>
   );
